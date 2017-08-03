8b2c5951a891d747e2d053b1ae38139bd0fad9d5
Ulrich Habel <espero7757@gmx.net>
2017-08-03 09:56:02 +0200

    USB_TMC class/module added

---
+++ RF-Lab_Win32/USB_TMC.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "USB_TMC.h"
+
+
+USB_TMC::USB_TMC()
+{
+}
+
+
+USB_TMC::~USB_TMC()
+{
+}
+
---
+++ RF-Lab_Win32/USB_TMC.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* @see https://github.com/python-ivi/python-usbtmc/blob/master/usbtmc/usbtmc.py */
+
+#include "USB_TMC.h"
+
+
+class USB_TMC
+{
+public:
+	USB_TMC();
+	~USB_TMC();
+};
+
